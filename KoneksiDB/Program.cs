e14e2ee38cdf892c9087236300db849b955d89b8
Ryvqi <rifqikhabibi03@gmail.com>
2024-03-08 10:51:54 +0700

    finish

--- KoneksiDB/Program.cs
+++ KoneksiDB/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
+using System.Linq.Expressions;
 
 namespace KoneksiDB
 {
@@ -11,6 +13,84 @@ namespace KoneksiDB
     {
         static void Main(string[] args)
         {
+            string strKoneksi = "";
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1, Koneksi Menggunakan Windows Authentication");
+                    Console.WriteLine("2. Koneksi Menggunakan SQL Server Authentication");
+                    Console.WriteLine("3, Exit");
+                    Console.WriteLine("\nEnter your Choice (1-3): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                try
+                                {
+                                    strKoneksi = "Data Source = METEORITE\\SQL2019; " +
+                                        "Initial Catalog = Pendataan Kematian; Integrated Security = True;";
+                                    SqlConnection koneksi = new SqlConnection();
+                                    koneksi.ConnectionString = strKoneksi;
+                                    koneksi.Open();
+                                    if (koneksi.State == ConnectionState.Open)
+                                    {
+                                        koneksi.Close();
+                                    }
+                                    Console.WriteLine("Koneksi Berhasil");
+                                    Console.ReadLine();
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Periksa Kembali Server Anda!\n" + ex.Message);
+                                    Console.ReadLine();
+
+                                }
+                            }
+                            break;
+                        case '2':
+                            {
+                                try
+                                {
+                                    strKoneksi = "Data Source = METEORITE\\SQL2019; " +
+                                        "Initial Catalog = Pendataan Kematian;User ID = sa; Password = meteorite";
+                                    SqlConnection koneksi = new SqlConnection();
+                                    koneksi.ConnectionString = strKoneksi;
+                                    koneksi.Open();
+                                    if (koneksi.State == ConnectionState.Open)
+                                    {
+                                        koneksi.Close();
+                                    }
+                                    Console.WriteLine("Koneksi Berhasil");
+                                    Console.ReadLine();
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Prikasa Kembali Server Anda!\n" + ex.Message);
+                                    Console.ReadLine();
+
+                                }
+                            }
+                            break;
+                        case '3':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("\nOpsi tidak valid");
+                                break;
+                            }
+                    }
+                    Console.Clear();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\nPeriksa angka yang dimasukkan. \n" + e.Message.ToString());
+                }
+            }
         }
     }
 }
+
